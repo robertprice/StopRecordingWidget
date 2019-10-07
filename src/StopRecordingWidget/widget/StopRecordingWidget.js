3a4cbcf943084e1969b4c3082a663fd6ad311302
Robert Price <robert.price@gmail.com>
2019-10-07 15:43:16 +0100

    First commit of the StopRecordingWidget

---
+++ src/StopRecordingWidget/widget/StopRecordingWidget.js
@@ -0,0 +1,73 @@
+define([
+    "dojo/_base/declare",
+    "mxui/widget/_WidgetBase",
+
+    "mxui/dom",
+    "dojo/dom",
+    "dojo/dom-prop",
+    "dojo/dom-geometry",
+    "dojo/dom-class",
+    "dojo/dom-style",
+    "dojo/dom-construct",
+    "dojo/_base/array",
+    "dojo/_base/lang",
+    "dojo/text",
+    "dojo/html",
+    "dojo/_base/event",
+
+
+], function (declare, _WidgetBase, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, lang, dojoText, dojoHtml, dojoEvent) {
+    "use strict";
+
+    return declare("StopRecordingWidget.widget.StopRecordingWidget", [ _WidgetBase ], {
+
+        // Parameter
+        delayMicroseconds: 3000,
+
+        // Internal variables.
+        _handles: null,
+        _contextObj: null,
+
+        constructor: function () {
+            this._handles = [];
+        },
+
+        postCreate: function () {
+            logger.debug(this.id + ".postCreate");
+        },
+
+        update: function (obj, callback) {
+            logger.debug(this.id + ".update");
+
+            window.setTimeout(function() {
+                document.title = "Stop Recording...";
+            }, this.delayMicroseconds);
+
+            this._contextObj = obj;
+            this._updateRendering(callback);
+        },
+
+        resize: function (box) {
+            logger.debug(this.id + ".resize");
+        },
+
+        uninitialize: function () {
+            logger.debug(this.id + ".uninitialize");
+        },
+
+        _updateRendering: function (callback) {
+            logger.debug(this.id + "._updateRendering");
+            this._executeCallback(callback, "_updateRendering");
+        },
+
+        // Shorthand for executing a callback, adds logging to your inspector
+        _executeCallback: function (cb, from) {
+            logger.debug(this.id + "._executeCallback" + (from ? " from " + from : ""));
+            if (cb && typeof cb === "function") {
+                cb();
+            }
+        }
+    });
+});
+
+require(["StopRecordingWidget/widget/StopRecordingWidget"]);
